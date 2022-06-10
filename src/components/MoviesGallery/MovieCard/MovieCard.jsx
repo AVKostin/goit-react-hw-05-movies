39c098772a2a754a3e5d21543e55e05395f05d95
Alexander Kostin <asurv2@gmail.com>
2022-06-10 03:58:49 +0300

    Update MovieCard.jsx

--- src/components/MoviesGallery/MovieCard/MovieCard.jsx
+++ src/components/MoviesGallery/MovieCard/MovieCard.jsx
@@ -1,25 +1,27 @@
 import PropTypes from "prop-types";
 import { Wrapper, Thumb, Picture, Title, Genres } from "./MovieCard.module";
 import NoPoster from "../../Cast/CastItem/No-photo-m.png";
-import { CardsDescription } from '../../MovieDetails/MovieDetails.styled'
+import { CardsDescription, RatingAccent, Rating } from '../../MovieDetails/MovieDetails.styled'
 
-const MovieCard = ({ title, posterPath, genres }) => (
+const MovieCard = ({ title, poster_path, genres, release_date, vote_average }) => {
+  return (
+<>
   <Wrapper>
     <Thumb>
-      {posterPath !== null ? (
+      {poster_path !== null ? (
         <picture>
           <source
-            srcSet={`https://themoviedb.org/t/p/w500${posterPath}`}
+            srcSet={`https://themoviedb.org/t/p/w500${poster_path}`}
             media="(min-width: 1024px)"
           />
           <source
-            srcSet={`https://themoviedb.org/t/p/w342${posterPath}`}
+            srcSet={`https://themoviedb.org/t/p/w342${poster_path}`}
             media="(min-width: 768px)"
           />
           <Picture
             alt={title}
             loading="lazy"
-            src={`https://themoviedb.org/t/p/w342${posterPath}`}
+            src={`https://themoviedb.org/t/p/w342${poster_path}`}
           />
         </picture>
       ) : (
@@ -27,17 +29,25 @@ const MovieCard = ({ title, posterPath, genres }) => (
       )}
     </Thumb>
       <CardsDescription>
-        <Title>{title}</Title>
-        <Genres>{genres}</Genres>
+        <Title>{title}<span> |</span>{release_date}</Title>
+      <Genres>{genres}</Genres>
+        <Rating>
+          <RatingAccent>{vote_average}</RatingAccent>
+        </Rating>
+
       </CardsDescription>
-    </Wrapper>
+      </Wrapper>
+</>
 );
+}
 
 MovieCard.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  posterPath: PropTypes.string,
+  poster_path: PropTypes.string,
   genres: PropTypes.string,
+  release_date: PropTypes.number.isRequired,
+  vote_average: PropTypes.number.isRequired,
 };
 
 export default MovieCard;
