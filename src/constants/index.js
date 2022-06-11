443856aa318777ba79ff188e66df0a68d04c77e4
Alexander Kostin <asurv2@gmail.com>
2022-06-11 23:54:52 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,8 @@
-export { NO_IMAGE } from './path';
-export { IMAGE_PATH_ORIGINAL } from './path';
-export { IMAGE_PATH_POSTER } from './path';
-export { IMAGE_PATH_PROFILE } from './path';
+export { NO_IMAGE } from './imagePosterPath';
+export { IMAGE_PATH_ORIGINAL } from './imagePosterPath';
+export { IMAGE_PATH_POSTER } from './imagePosterPath';
+export { IMAGE_PATH_PROFILE } from './imagePosterPath';
+export { URL_POSTER } from './imagePosterPath';
+export { BASE_URL } from './imagePosterPath';
+export { api_key } from './imagePosterPath';
+export { theme } from './colors';
