3cd800ccb0cd692abbe5d5d5a0b107e7fc978076
Alexander Kostin <asurv2@gmail.com>
2022-06-12 00:02:03 +0300

    Create CastItem.jsx

---
+++ src/pages/Cast/CastItem/CastItem.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import { Wrapper, Photo, NoPhoto, Title, Subtitle } from "./CastItem.styled";
+import { URL_POSTER } from '../../../constants/index'
+import noPhoto from "../CastItem/No-photo-m.png";
+
+export default function CastItem({ name, character, photo }) {
+  return (
+    <Wrapper>
+      {photo ? (
+        <Photo src={`${URL_POSTER}w185${photo}`} alt={name} />
+      ) : (
+        <NoPhoto src={noPhoto} alt={name} />
+      )}
+
+      <Title>{name}</Title>
+      <Subtitle>{character}</Subtitle>
+    </Wrapper>
+  );
+};
+
+CastItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  character: PropTypes.string.isRequired,
+  photo: PropTypes.string,
+};
