69d3c62c1efcf26680756b9ef42e8b0831e152b8
Alexander Kostin <asurv2@gmail.com>
2022-06-12 21:25:51 +0300

    Update checkPoster.jsx

--- src/utils/checkPoster.jsx
+++ src/utils/checkPoster.jsx
@@ -1,7 +1,7 @@
-import { IMAGE_PATH_POSTER, NO_IMAGE } from '../constants';
+import { IMAGE_POSTER_500, NO_IMAGE } from '../constants';
 
 export const checkPoster = poster_path => {
   return poster_path
-    ? (poster_path = IMAGE_PATH_POSTER + poster_path)
+    ? (poster_path = IMAGE_POSTER_500 + poster_path)
     : (poster_path = NO_IMAGE);
 };
