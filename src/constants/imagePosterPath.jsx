3394f62984bf4262c374a2d99dba07ac1fa5fb08
Alexander Kostin <asurv2@gmail.com>
2022-06-12 21:24:58 +0300

    Update imagePosterPath.jsx

--- src/constants/imagePosterPath.jsx
+++ src/constants/imagePosterPath.jsx
@@ -1,8 +1,8 @@
 export const NO_IMAGE =
   'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
 export const IMAGE_PATH_ORIGINAL = `https://image.tmdb.org/t/p/original`;
-export const IMAGE_PATH_POSTER = `https://image.tmdb.org/t/p/w500`;
-export const IMAGE_PATH_PROFILE = `https://image.tmdb.org/t/p/w185`;
+export const IMAGE_POSTER_500 = `https://image.tmdb.org/t/p/w500`;
+export const IMAGE_POSTER_185 = `https://image.tmdb.org/t/p/w185`;
 export const URL_POSTER = `https://themoviedb.org/t/p/`
 export  const BASE_URL = "https://api.themoviedb.org/3";
 export const api_key = "b3b50e370b6b6fb5970e86b3e5ccd8d7";
