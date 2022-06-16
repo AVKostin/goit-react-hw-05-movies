f18d1b33eb2d4c986f46d51c03bfd8011527312d
Alexander Kostin <asurv2@gmail.com>
2022-06-16 17:54:00 +0300

    Create Trailer.styled.jsx

---
+++ src/pages/Trailer/Trailer.styled.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { breakpoints } from "../../utils/breakPoints";
+
+export const List = styled.ul`
+    margin-top: 10px;
+    display: grid;
+    color: black;
+    gap: 20px;
+    grid-template-columns: 1fr;
+        @media ${breakpoints.tablet} {
+            grid-template-columns: 1fr 1fr;
+    }
+        @media ${breakpoints.laptop} {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    & img {
+        margin-top: 10px;
+        border-radius: 20px;
+    }
+`;
