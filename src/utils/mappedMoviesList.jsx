c9bbe07b6d686fe91258d5170a2f4303f3d8c712
Alexander Kostin <asurv2@gmail.com>
2022-06-16 21:35:47 +0300

    Delete mappedMoviesList.jsx

--- src/utils/mappedMoviesList.jsx
+++
@@ -1,9 +0,0 @@
-import { filteredGenres } from "./filteredGenres";
-
-export const mappedMoviesList = (genresList, list) =>
-  list.map((item) => ({
-    id: item.id,
-    title: item.title,
-    poster_path: item.poster_path,
-    genres: filteredGenres(genresList, item.genre_ids),
-  }));
