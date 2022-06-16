46706c5012228511212372fc2efd728e5196ecd9
Alexander Kostin <asurv2@gmail.com>
2022-06-16 21:35:15 +0300

    Create handleAvatarLink.js

---
+++ src/utils/handleAvatarLink.js
@@ -0,0 +1,9 @@
+import { URL_POSTER } from '../constants/index'
+
+export const handleAvatarLink = (link) => {
+  if (!link) return "";
+
+  if (link.includes("http")) return link.substring(1);
+
+  return `${URL_POSTER}w45${link}`;
+};
