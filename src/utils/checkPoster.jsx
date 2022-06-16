015aa1b5729557fbc2dfc1fba4b18adc9dfc206e
Alexander Kostin <asurv2@gmail.com>
2022-06-16 21:33:57 +0300

    Delete checkPoster.jsx

--- src/utils/checkPoster.jsx
+++
@@ -1,7 +0,0 @@
-import { IMAGE_POSTER_500, NO_IMAGE } from '../constants';
-
-export const checkPoster = poster_path => {
-  return poster_path
-    ? (poster_path = IMAGE_POSTER_500 + poster_path)
-    : (poster_path = NO_IMAGE);
-};
