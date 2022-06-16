9836215cec8230bb74a66a92c8b05497a7767a1f
Alexander Kostin <asurv2@gmail.com>
2022-06-16 21:33:50 +0300

    Delete checkAvatarPath.jsx

--- src/utils/checkAvatarPath.jsx
+++
@@ -1,7 +0,0 @@
-import { IMAGE_POSTER_185, NO_IMAGE } from '../constants';
-
-export const checkAvatarPath = avatar => {
-  if (!avatar) return (avatar = NO_IMAGE);
-  if (avatar.includes(`https`)) return (avatar = avatar.slice(1));
-  return (avatar = IMAGE_POSTER_185 + avatar);
-};
