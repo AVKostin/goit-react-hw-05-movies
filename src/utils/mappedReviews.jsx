34d5e6ca3caf236d9c8e691c901ec365d9b0b68b
Alexander Kostin <asurv2@gmail.com>
2022-06-16 21:35:37 +0300

    Delete mappedReviews.jsx

--- src/utils/mappedReviews.jsx
+++
@@ -1,9 +0,0 @@
-import { handleAvatarLink } from "./handleAvatarLink";
-
-export const mappedReviews = (list) =>
-  list.map((item) => ({
-    id: item.id,
-    name: item.author,
-    avatar: handleAvatarLink(item.author_details.avatar_path),
-    text: item.content,
-  }));
