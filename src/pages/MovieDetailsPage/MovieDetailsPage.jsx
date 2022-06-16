1b83c59724c572de4fd2b2f868fcc6c89c07faf2
Alexander Kostin <asurv2@gmail.com>
2022-06-16 21:38:04 +0300

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,16 +3,16 @@ import { Routes, Route, Outlet, useParams } from "react-router-dom";
 import Cast from "../Cast";
 import Reviews from "../Reviews";
 import Section from "../../UI/section";
-import Trailers from "./Trailer";
+import Trailers from "../Trailer";
 import Container from "../../UI/container";
-import { MovieDetailsItem } from "../../utils/movieDetailsItem";
 import { fetchFilmDetails } from "../../Services/movieApi";
-import { MovieDetails } from "../MovieDetails/MovieDetails";
+import MovieDetails from "../MovieDetails/MovieDetails";
 import { Loading } from "notiflix/build/notiflix-loading-aio";
 import NavigateBackButton from "../../components/NavigateBackButton";
 import MovieDetailsNavigation from "../../components/MovieDetailsNavigation";
+import { movieDetails } from "../../utils/movieDetails";
 
-export default function MovieDetailsPage (){
+const MovieDetailsPage = () => {
   const { movieID } = useParams();
   const [movieData, setMovieData] = useState(null);
 
@@ -25,13 +25,14 @@ export default function MovieDetailsPage (){
       messageColor: "#00FF00",
     });
 
-    fetchFilmDetails(movieID).then((data) => {
-      setMovieData(MovieDetailsItem(data));
+    fetchFilmDetails(movieID)
+      .then((data) => {
+      setMovieData(movieDetails(data));
     });
     Loading.remove();
   }, [movieID]);
 
-  return (
+  return <>
     <Section>
       <Container>
         <NavigateBackButton />
@@ -50,6 +51,8 @@ export default function MovieDetailsPage (){
         </Routes>
       </Container>
     </Section>
-  );
+  </>
+
 };
 
+export default MovieDetailsPage;
