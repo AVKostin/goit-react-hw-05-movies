aa2498a2e9927b1eabf0a536f959bf28eaf5fc3d
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:00 +0300

    Update MainLayout.jsx

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,23 +1,24 @@
-import { Outlet } from "react-router-dom";
-import {Main} from "./MainLayout.styled";
-import { ScrollToTop } from "react-to-top";
-import AppHeader from "../AppHeader";
+import { Outlet } from 'react-router-dom';
+import { Main } from './MainLayout.styled';
+import { ScrollToTop } from 'react-to-top';
+import AppHeader from '../AppHeader';
 
 const MainLayout = () => (
-  <div>
-    <AppHeader />
-    <Main>
-      <Outlet />
-    </Main>
-    <ScrollToTop
-      bgColor="#FF0000"
-      strokeEmptyColor="#00FF00"
-      strokeWidth={5}
-      symbolSize={25}
-      symbol="&#9650;"
-      size={55}
-    />
-  </div>
+	<div>
+		<AppHeader />
+		<Main>
+			<Outlet />
+		</Main>
+
+		<ScrollToTop
+			bgColor="#FF0000"
+			strokeEmptyColor="#00FF00"
+			strokeWidth={5}
+			symbolSize={25}
+			symbol="&#9650;"
+			size={55}
+		/>
+	</div>
 );
 
 export default MainLayout;
