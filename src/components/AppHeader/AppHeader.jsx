da4e82f77b940e2427f3536e940e49e322b746dd
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:39 +0300

    Update AppHeader.jsx

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,13 +1,13 @@
-import {Wrapper} from "./AppHeader.styled";
-import Container from "../../UI/container";
-import Navigation from "../../components/Navigation";
+import { Wrapper } from './AppHeader.styled';
+import Container from '../../UI/container';
+import Navigation from '../../components/Navigation';
 
-export default function Header(){
-  return (
-    <Wrapper>
-      <Container>
-        <Navigation />
-      </Container>
-    </Wrapper>
-  );
-};
+export default function Header() {
+	return (
+		<Wrapper>
+			<Container>
+				<Navigation />
+			</Container>
+		</Wrapper>
+	);
+}
