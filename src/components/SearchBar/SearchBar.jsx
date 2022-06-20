f3a5c635fe762c7ad1a149769076bcff05a883d8
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:21 +0300

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,42 +1,43 @@
-import { useState } from "react";
-import { Form, Input } from "./SearchBar.styled";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-import AccentButton from "../../UI/AccentButton";
+import { useState } from 'react';
+import { Form, Input } from './SearchBar.styled';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import AccentButton from '../../UI/AccentButton';
 
 Notify.init({
 	position: 'center-top',
 	width: '400px',
-  fontSize: '18px',
-  cssAnimationStyle: "from-right"
+	fontSize: '18px',
+	cssAnimationStyle: 'from-right',
 });
 
 const SearchForm = ({ onSearch }) => {
-  const [value, setValue] = useState("");
+	const [value, setValue] = useState('');
 
-  const handleSearch = (e) => {
-    e.preventDefault();
+	const handleSearch = e => {
+		e.preventDefault();
 
-    const query = value.trim();
-    if (query === "") {
-      Notify.failure("Please enter something");
-    }
-    if (query) {
-      onSearch(query);
-      setValue("");
-    }
-  };
+		const query = value.trim();
+		if (query === '') {
+			Notify.failure('Please enter something');
+		}
+		if (query) {
+			onSearch(query);
+			setValue('');
+		}
+	};
 
-  return <>
-      <Form onSubmit={handleSearch}>
-        <Input
-          value={value}
-          placeholder="Search for movies"
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <AccentButton title="Find" type="submit" />
-      </Form>
-    </>
-
+	return (
+		<>
+			<Form onSubmit={handleSearch}>
+				<Input
+					value={value}
+					placeholder="Search for movies"
+					onChange={e => setValue(e.target.value)}
+				/>
+				<AccentButton title="Find" type="submit" />
+			</Form>
+		</>
+	);
 };
 
 export default SearchForm;
