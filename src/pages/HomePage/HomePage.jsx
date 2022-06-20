213a00f6c658e4fd493d0e35122f33fe6c1e2c04
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:18 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,37 +1,37 @@
-import { fetchTrends } from "../../Services/movieApi";
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import MoviesGallery from "../MoviesGallery/MoviesGallery";
-import { mappedMoviesList } from "../../utils/mappedMoviesList";
-import Container from "../../UI/container";
-import Section from "../../UI/section";
+import { fetchTrends } from '../../Services/movieApi';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import MoviesGallery from '../MoviesGallery/MoviesGallery';
+import { mappedMoviesList } from '../../utils/mappedMoviesList';
+import Container from '../../UI/container';
+import Section from '../../UI/section';
 
 export default function HomePage({ genresList }) {
-  const [moviesList, setMoviesList] = useState([]);
+	const [moviesList, setMoviesList] = useState([]);
 
-  useEffect(() => {
-    if (genresList.length)
-      fetchTrends().then((data) => {
-          setMoviesList(mappedMoviesList(genresList, data.results));
-      });
-  }, [genresList]);
+	useEffect(() => {
+		if (genresList.length)
+			fetchTrends().then(data => {
+				setMoviesList(mappedMoviesList(genresList, data.results));
+			});
+	}, [genresList]);
 
-  return <>
-    <Section>
-      <Container>
-        <MoviesGallery list={moviesList} />
-      </Container>
-    </Section>
-  </>
-
-
-};
+	return (
+		<>
+			<Section>
+				<Container>
+					<MoviesGallery list={moviesList} />
+				</Container>
+			</Section>
+		</>
+	);
+}
 
 HomePage.propTypes = {
-  genresList: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+	genresList: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			name: PropTypes.string.isRequired,
+		})
+	).isRequired,
 };
