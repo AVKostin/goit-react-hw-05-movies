7912afbfcbb008d5c33c369be9272b35449bb1b0
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:24 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,31 +1,26 @@
-import {
-  Wrapper,
-  Menu,
-  Item,
-  StyledLink,
-} from "./Navigation.styled";
+import { Wrapper, Menu, Item, StyledLink } from './Navigation.styled';
 
 const Navigation = () => (
-  <Wrapper>
-    <Menu>
-      <Item>
-        <StyledLink
-          className={({ isActive }) => (isActive ? "active" : null)}
-          to="/"
-        >
-          Home
-        </StyledLink>
-      </Item>
-      <Item>
-        <StyledLink
-          className={({ isActive }) => (isActive ? "active" : null)}
-          to="/movies"
-        >
-          Movies
-        </StyledLink>
-      </Item>
-    </Menu>
-  </Wrapper>
+	<Wrapper>
+		<Menu>
+			<Item>
+				<StyledLink
+					className={({ isActive }) => (isActive ? 'active' : null)}
+					to="/"
+				>
+					Home
+				</StyledLink>
+			</Item>
+			<Item>
+				<StyledLink
+					className={({ isActive }) => (isActive ? 'active' : null)}
+					to="/movies"
+				>
+					Movies
+				</StyledLink>
+			</Item>
+		</Menu>
+	</Wrapper>
 );
 
 export default Navigation;
