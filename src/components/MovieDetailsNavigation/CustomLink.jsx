0f74a3bdffa96ef1a87f1b8c92867dbaa96828f3
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:33 +0300

    Update CustomLink.jsx

--- src/components/MovieDetailsNavigation/CustomLink.jsx
+++ src/components/MovieDetailsNavigation/CustomLink.jsx
@@ -1,14 +1,13 @@
-import { useMatch, useResolvedPath } from "react-router-dom";
-import { StyledLink } from "./CustomLink.styled";
+import { useMatch, useResolvedPath } from 'react-router-dom';
+import { StyledLink } from './CustomLink.styled';
 
 export default function CustomLink({ children, to, ...props }) {
-  let resolved = useResolvedPath(to);
-  let match = useMatch({ path: resolved.pathname, end: true });
+	let resolved = useResolvedPath(to);
+	let match = useMatch({ path: resolved.pathname, end: true });
 
-  return (
-    <StyledLink className={match ? "active" : null}
-      to={to} {...props}>
-      {children}
-    </StyledLink>
-  );
-};
+	return (
+		<StyledLink className={match ? 'active' : null} to={to} {...props}>
+			{children}
+		</StyledLink>
+	);
+}
