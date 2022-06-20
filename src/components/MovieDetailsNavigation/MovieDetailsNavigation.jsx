705456710e79bdb06bbd5ca8b827eae3b040458d
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:36 +0300

    Update MovieDetailsNavigation.jsx

--- src/components/MovieDetailsNavigation/MovieDetailsNavigation.jsx
+++ src/components/MovieDetailsNavigation/MovieDetailsNavigation.jsx
@@ -1,28 +1,26 @@
-import PropTypes from "prop-types";
-import { LinksMenu, LinksItem } from "./MovieDetailsNavigation.styled";
+import PropTypes from 'prop-types';
+import { LinksMenu, LinksItem } from './MovieDetailsNavigation.styled';
 
-import CustomLink from "./CustomLink";
+import CustomLink from './CustomLink';
 
 export default function MovieDetailsNavigation({ id }) {
-  return (
-    <LinksMenu>
-      <LinksItem>
-          <CustomLink to={`/movies/${id}/credits`}>Credits</CustomLink>
-      </LinksItem>
+	return (
+		<LinksMenu>
+			<LinksItem>
+				<CustomLink to={`/movies/${id}/credits`}>Credits</CustomLink>
+			</LinksItem>
 
-      <LinksItem>
-        <CustomLink to={`/movies/${id}/reviews`}>Reviews</CustomLink>
-      </LinksItem>
+			<LinksItem>
+				<CustomLink to={`/movies/${id}/reviews`}>Reviews</CustomLink>
+			</LinksItem>
 
-      <LinksItem>
-        <CustomLink to={`/movies/${id}/trailer`}>Trailer</CustomLink>
-      </LinksItem>
-
-      </LinksMenu>
-  );
-};
+			<LinksItem>
+				<CustomLink to={`/movies/${id}/trailer`}>Trailer</CustomLink>
+			</LinksItem>
+		</LinksMenu>
+	);
+}
 
 MovieDetailsNavigation.propTypes = {
-  id: PropTypes.string.isRequired,
+	id: PropTypes.string.isRequired,
 };
-
