db6f7f6e45bff55b7884afbcf032e7f0d690360c
Alexander Kostin <asurv2@gmail.com>
2022-06-20 13:02:05 +0300

    Update MoviesGallery.jsx

--- src/pages/MoviesGallery/MoviesGallery.jsx
+++ src/pages/MoviesGallery/MoviesGallery.jsx
@@ -1,30 +1,30 @@
-import { Link, useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Wrapper, Item } from "./MoviesGallery.styled";
-import  MovieCard  from "../MoviesGallery/MovieCard/";
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Wrapper, Item } from './MoviesGallery.styled';
+import MovieCard from '../MoviesGallery/MovieCard/';
 
-export default function MoviesGallery ({ list }){
-  const location = useLocation();
+export default function MoviesGallery({ list }) {
+	const location = useLocation();
 
-  const renderGallery = () =>
-    list.map((item) => (
-      <Item key={item.id}>
-        <Link to={`/movies/${item.id}`} state={{ from: location }}>
-          <MovieCard {...item} />
-        </Link>
-      </Item>
-    ));
+	const renderGallery = () =>
+		list.map(item => (
+			<Item key={item.id}>
+				<Link to={`/movies/${item.id}`} state={{ from: location }}>
+					<MovieCard {...item} />
+				</Link>
+			</Item>
+		));
 
-  return <Wrapper>{list.length ? renderGallery() : null}</Wrapper>;
-};
+	return <Wrapper>{list.length ? renderGallery() : null}</Wrapper>;
+}
 
 MoviesGallery.propTypes = {
-  list: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      posterPath: PropTypes.string,
-      genres: PropTypes.string,
-    })
-  ),
+	list: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			title: PropTypes.string.isRequired,
+			posterPath: PropTypes.string,
+			genres: PropTypes.string,
+		})
+	),
 };
