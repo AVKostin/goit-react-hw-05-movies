96c95a13c7b71427714893f6c709452c1a413526
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 16:31:03 +0300

    refactoring and clear code

--- src/components/MovieDetailsNavigation/MovieDetailsNavigation.styled.jsx
+++ src/components/MovieDetailsNavigation/MovieDetailsNavigation.styled.jsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 
 export const LinksMenu = styled.ul`
-  // margin-top: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 10px;
   background-color: ${(p) => p.theme.cardBGColor};
-  // border-radius: 10px;
 `;
 
 export const LinksItem = styled.li`
