cb2cde8ea7cead0071a4b70a2cd74237d29c2406
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 16:30:20 +0300

    refactoring and clear code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'modern-normalize/modern-normalize.css';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './constants/colors';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import "modern-normalize/modern-normalize.css";
+import { theme } from "./constants/colors";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename="/goit-react-hw-05-movies/">
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
 );
